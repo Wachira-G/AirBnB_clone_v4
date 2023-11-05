4e5dac0207f1ada43696cd0d69c0780477c2dfa2
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-05 13:19:44 +0300

    add task 5 files: filter places by amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).prop('checked')) {
+      checkedAmenities[amenityID] = amenityName;
+    } else {
+      delete checkedAmenities[amenityID];
+    }
+
+    const h4 = $('.amenities h4');
+    h4.text(Object.values(checkedAmenities).join(', '));
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+
+  function generateArticleHTML(place) {
+    return `<article>
+             <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${'$' + place.price_by_night}</div>
+             </div>
+             <div class="information">
+                <div class="max_guest">${place.max_guest + (place.max_guest > 1 ? ' Guests' : ' Guest')} </div>
+                <div class="number_rooms">${place.number_rooms + (place.number_rooms > 1 ? ' Bedrooms' : ' Bedroom')} </div>
+                <div class="number_bathrooms">${place.number_bathrooms + (place.number_bathrooms > 1 ? ' Bathrooms' : ' Bathroom')}</div>
+             </div>
+             <div class="description">${place.description}</div>
+           </article>`;
+  }
+
+  function fetchPlaces(data) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      type: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify(data),
+      success: function (response) {
+        $('section.places').empty();
+        response.forEach(function (place) {
+          $('section.places').append(generateArticleHTML(place));
+        });
+      },
+      error: function (error) {
+        console.error(error);
+      },
+    });
+  }
+
+  fetchPlaces({}); // Fetch all places on page load
+
+  $('button').on('click', function () {
+    fetchPlaces({ amenities: Object.keys(checkedAmenities) }); // Fetch places with selected amenities on button click
+  });
+});

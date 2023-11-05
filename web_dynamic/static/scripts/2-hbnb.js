73e2857c71358a8630a1845ac9b702e900061b5b
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-05 07:45:16 +0300

    add task 3 files: api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// task 2 js script
+
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).prop('checked')) {
+      checkedAmenities[amenityID] = amenityName;
+    } else {
+      delete checkedAmenities[amenityID];
+    }
+
+    const h4 = $('.amenities h4');
+    h4.text(Object.values(checkedAmenities).join(', '));
+  });
+
+  $.get('http://localhost:5001/api/v1/status/', function(data, textStatus){
+    if (textStatus == "success"){
+      if(data.status === 'OK'){
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
